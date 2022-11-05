7923c829edff2a5f56d9376ab55b6f35b12ee608
Snickbit <snickbit@users.noreply.github.com>
2022-11-04 23:01:21 -0400

    fix: json parsing

--- src/main.sh
+++ src/main.sh
@@ -11,17 +11,17 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 json=${1-}
 [[ -n $json ]] || die "No JSON file specified"
 
-NPM_TOKEN=$(jq -r '.NPM_TOKEN' "$json")
-GITHUB_TOKEN=$(jq -r '.GITHUB_TOKEN' "$json")
-NPM_REGISTRY=$(jq -r '.NPM_REGISTRY' "$json")
-PUBLISH_SCRIPT=$(jq -r '.PUBLISH_SCRIPT' "$json")
-PRERELEASE_SCRIPTS=$(jq -r '.PRERELEASE_SCRIPTS' "$json")
-NO_BAIL=$(jq -r '.NO_BAIL' "$json")
-BAIL_ON_MISSING=$(jq -r '.BAIL_ON_MISSING' "$json")
-DRY_RUN=$(jq -r '.DRY_RUN' "$json")
-DEBUG=$(jq -r '.DEBUG' "$json")
-CONFIG_ONLY=$(jq -r '.CONFIG_ONLY' "$json")
-PRERELEASE_ONLY=$(jq -r '.PRERELEASE_ONLY' "$json")
+NPM_TOKEN=$(echo "$json" | jq -r '.NPM_TOKEN')
+GITHUB_TOKEN=$(echo "$json" | jq -r '.GITHUB_TOKEN')
+NPM_REGISTRY=$(echo "$json" | jq -r '.NPM_REGISTRY')
+PUBLISH_SCRIPT=$(echo "$json" | jq -r '.PUBLISH_SCRIPT')
+PRERELEASE_SCRIPTS=$(echo "$json" | jq -r '.PRERELEASE_SCRIPTS')
+NO_BAIL=$(echo "$json" | jq -r '.NO_BAIL')
+BAIL_ON_MISSING=$(echo "$json" | jq -r '.BAIL_ON_MISSING')
+DRY_RUN=$(echo "$json" | jq -r '.DRY_RUN')
+DEBUG=$(echo "$json" | jq -r '.DEBUG')
+CONFIG_ONLY=$(echo "$json" | jq -r '.CONFIG_ONLY')
+PRERELEASE_ONLY=$(echo "$json" | jq -r '.PRERELEASE_ONLY')
 
 echo "PUBLISH_SCRIPT: $PUBLISH_SCRIPT"
 echo "PRERELEASE_SCRIPTS: $PRERELEASE_SCRIPTS"
